1c7153e6044b46ab28aa7f1f9441c244ed2cf512
Anthony Tran <quanghust96@gmail.com>
2023-04-26 14:18:51 +0700

    hoan thien logic voi redux core

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,15 +1,16 @@
 import { useState } from 'react';
 import { Col, Row, Input, Button, Select, Tag } from 'antd';
 import Todo from '../Todo';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addTodo } from '../../redux/actions';
 import {v4 as uuidv4} from 'uuid';
+import { todosRemainingSelector } from '../../redux/selectors';
 
 export default function TodoList() {
   const [todoName, setTodoName]=useState('');
   const [priority,setPriority]=useState('Medium');
   const dispatch=useDispatch();
-
+  const todoList=useSelector(todosRemainingSelector);
   const handleInputChange=(e)=>{
     setTodoName(e.target.value);
   }
@@ -29,9 +30,8 @@ export default function TodoList() {
   return (
     <Row style={{ height: 'calc(100% - 40px)' }}>
       <Col span={24} style={{ height: 'calc(100% - 40px)', overflowY: 'auto' }}>
-        <Todo name='Learn React' prioriry='High' />
-        <Todo name='Learn Redux' prioriry='Medium' />
-        <Todo name='Learn JavaScript' prioriry='Low' />
+        {console.log(todoList)}
+        {todoList.map((todo)=><Todo name={todo.name} id={todo.id} prioriry={todo.priority} key={todo.id} completed={todo.completed}/> )}
       </Col>
       <Col span={24}>
         <Input.Group style={{ display: 'flex' }} compact>
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,27 +1,11 @@
-const initState={
-    filters:{
-        search:'',
-        status: 'All',
-        priority: [],
-    },
-    todoList:[
-        {id: 1, name: 'Learn Yoga', completed: false, priority: 'Medium'},
-        {id: 2, name: 'Learn Redux', completed: true, priority: 'High'},
-        {id: 3, name: 'Learn JS', completed: false, priority: 'Low'}
-    ]
-}
-const rootReducer=(state=initState ,action)=>{
-    console.log({state,action});
-    switch(action.type){
-        case 'todoList/addTodo':
-            return {
-                ...state,
-                todoList: [...state.todoList,
-                {id: 5, name: 'Learn Football', completed: false, priority: 'Medium'}
-                ]
-            }
-        default: return state;
-    }
-}
+import {combineReducers} from 'redux'
+
+import filtersReducer from '../components/Filters/FiltersSlice';
+import todoListReducer from '../components/TodoList/TodosSlice';
+
+const rootReducer=combineReducers({
+    filters: filtersReducer,
+    todoList: todoListReducer
+})
 
 export default rootReducer;
